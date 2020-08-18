382e0d0e149c343ca513a19f661d3633edae8288
Vanessa Cendales <vanecendales@gmail.com>
2020-08-18 13:07:36 -0700

    loading animation logo EDUAC

---
+++ eduac animationTests/eduac_animationTests.swift
@@ -0,0 +1,34 @@
+//
+//  eduac_animationTests.swift
+//  eduac animationTests
+//
+//  Created by user179774 on 8/18/20.
+//  Copyright © 2020 user179774. All rights reserved.
+//
+
+import XCTest
+@testable import eduac_animation
+
+class eduac_animationTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
